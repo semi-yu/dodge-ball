6b9dd96ebbafdbfc6e67eca99549f210ee638d79
armedHeistKino <urban.manul.1919@gmail.com>
2024-12-16 16:33:32 +0900

    feat: Add management code for game over

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,16 +1,38 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public GameObject gameoverText;
+    public Text timeText;
+    public Text recordText;
+
+    private float surviveTime;
+    private bool isGameOver;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        surviveTime = 0f;
+        isGameover = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isGameover) {
+            surviveTime += Time.deltaTime;
+            timeText.text = "Time: " + (int) surviveTime;
+        }
+        else {
+            if (Input.GetKeyDown(KeyCode.R)) {
+                SceneManager.LoadScene("SampleScene");
+            }
+        }
+    }
+
+    void EndGame() {
+        isGameover = true;
+        gameoverText.SetActive(true);
     }
 }
